da67e22076c505a07c4facd69c4ea0ee7908c05c
Patrik Bego <pbego@itrsgroup.com>
2026-05-19 16:21:09 +0100

    Initial commit: Embabel Ollama simple demo

--- src/test/java/com/example/simpledemo/SimpleDemoApplicationTests.java
+++ src/test/java/com/example/simpledemo/SimpleDemoApplicationTests.java
@@ -3,7 +3,11 @@ package com.example.simpledemo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+    "embabel.agent.shell.interactive.enabled=false",
+    "spring.shell.interactive.enabled=false",
+    "spring.shell.noninteractive.enabled=false"
+})
 class SimpleDemoApplicationTests {
 
   @Test
